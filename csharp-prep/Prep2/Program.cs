05d65d9026c8962445a5602f585afa7244821c65
Braydon Tolman <bctolman2@gmail.com>
2025-04-26 18:36:14 -0600

    prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,34 @@
 using System;
 
-class Program
+Console.WriteLine("Enter your grade percentage: ");
+int gradePercentage = int.Parse(Console.ReadLine());
+string letterGrade;
+if (gradePercentage >= 90)
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
-    }
+    letterGrade = "A";
+}
+else if (gradePercentage >= 80)
+{
+    letterGrade = "B";
+}
+else if (gradePercentage >= 70)
+{
+    letterGrade = "C";
+}
+else if (gradePercentage >= 60)
+{
+    letterGrade = "D";
+}
+else
+{
+    letterGrade = "F";
+}
+Console.WriteLine($"You got a(n) {letterGrade}");
+if (gradePercentage >= 70)
+{
+    Console.WriteLine("You passed! Congradulations!!!");
+}
+else
+{
+    Console.WriteLine("You did not pass this time. You'll do better next time!");
 }
