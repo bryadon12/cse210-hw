ee8de91577cac63a0f01459b5ec6e106fa4b2c87
Braydon Tolman <bctolman2@gmail.com>
2025-04-29 10:55:03 -0600

    Started prep 3 and added write line statement

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic Number?");
+        int magicNumber = int.Parse(Console.ReadLine());
+        Console.WriteLine("What is your guess?");
+        int guess = int.Parse(Console.ReadLine());
+        if (guess == magicNumber)
+        {
+            Console.WriteLine("You got it!!!");
+        }
+        else if (guess < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else
+        {
+            Console.WriteLine("Lower");
+        }
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,12 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Input your first name: ");
-        string firstName = Console.ReadLine();
-        Console.WriteLine($"Your first name is: {firstName}");
-        Console.Write("Input your last name: ");
-        string lastName = Console.ReadLine();
-        Console.WriteLine($"Your last name is: {lastName}");
-        Console.WriteLine($"Your  name is: {lastName}, {firstName} {lastName}");
+        Console.Write("Hey how are you?");
     }
 }
