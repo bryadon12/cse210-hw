7b66bf007b50c3668f363980b0f4616ae9e42aa9
Braydon Tolman <bctolman2@gmail.com>
2025-05-06 11:51:13 -0600

    Prep 5 and sandbox Circle Class

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        Console.Write("Welcome to the program!\nPlease enter your name: ");
+        string name = Console.ReadLine();
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        Console.WriteLine($" Brother {name}, the square of your number is {number*number}");
     }
+//  Welcome to the program!
+// Please enter your name: Brother Burton
+// Please enter your favorite number: 42
+// Brother Burton, the square of your number is 1764
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,9 +1,36 @@
 using System;
+class Circle
+{
+    private double _radius;
 
+    public void SetRadius(double radius)
+    {
+        if (radius < 0)
+        {
+            Console.WriteLine("Error, radius must be > 0");
+            return;
+        }
+        _radius = radius;
+    }
+
+    public double GetRadius()
+    {
+        return _radius;
+    }
+
+    public double GetArea()
+    {
+        return Math.PI * _radius * _radius;
+    }
+}
 class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Hey how are you?");
+           // Console.WriteLine("Hello World!");
+           Circle myCircle = new Circle();
+           myCircle.SetRadius(10);
+           Console.WriteLine(myCircle.GetRadius());
+           Console.WriteLine(myCircle.GetArea());
     }
 }
