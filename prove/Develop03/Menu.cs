61b9fa69f29b54cc4113fd0c6f3be349ca0e44af
Braydon Tolman <bctolman2@gmail.com>
2025-06-03 09:02:20 -0600

    menu

--- prove/Develop03/Menu.cs
+++ prove/Develop03/Menu.cs
@@ -23,8 +23,13 @@ class Menu
         Console.WriteLine("What is the reference for your verse? \n Enter as Book Chapter:VerseStart-VerseEnd");
         String ReferenceString = Console.ReadLine();
         List<String> SplitBook = [.. ReferenceString.Split(" ")];
+        if (SplitBook.Count > 2)
+        {
+            SplitBook[0] = $"{SplitBook[0]} {SplitBook[1]}";
+            SplitBook.RemoveAt(1);
+        }
         List<String> SplitChapter = [.. SplitBook[1].Split(":")];
         List<String> SplitVerse = [.. SplitChapter[1].Split("-")];
-
+        Reference reference = new Reference(SplitVerse[0], 1, 2, 3);
     } 
 }
