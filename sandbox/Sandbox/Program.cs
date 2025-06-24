66c5d7b4b3a0cfc41210d2c1e8e3b7447ea76659
Braydon Tolman <bctolman2@gmail.com>
2025-06-24 11:49:09 -0600

    classes practice

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -3,19 +3,9 @@ class Program
 {
     static void Main(string[] args)
     {
-        Circle myCircle = new Circle(10);
-        // myCircle.SetRadius(10);
-        Console.WriteLine($"{myCircle.GetRadius()}");
-        // Circle myCircle2 = new Circle();
-        // myCircle2.SetRadius(20);
-        // Console.WriteLine($"{myCircle2.GetRadius()}");
-
-        // Console.WriteLine($"{myCircle.GetArea()}");
-        // Console.WriteLine($"{myCircle2.GetArea()}");
-
-        Cylinder myCylinder = new Cylinder(100, 8);
-        // myCylinder.SetHeight(10);
-        // myCylinder.SetCircle(myCircle);
-        Console.WriteLine($"{myCylinder.GetVolume()}");
+        Person myPerson = new Person("Bob", "Lemi", 34);
+        Console.WriteLine(myPerson.GetPersonInformation());
+        PoliceMan myPoliceman = new PoliceMan("Betty", "Croker", 23, "hammer");
+        Console.WriteLine(myPoliceman.GetPersonInformation());
     }
 }
---
+++ sandbox/Sandbox/policeMan.cs
@@ -0,0 +1,8 @@
+class PoliceMan : Person
+{
+    private string _weapons;
+    public PoliceMan(string firstName, string lastName, int age, string weapons) : base(firstName, lastName, age)
+    {
+        _weapons = weapons;
+    }
+}
